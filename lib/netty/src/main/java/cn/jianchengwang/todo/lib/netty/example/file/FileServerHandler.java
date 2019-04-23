0f9daf5ebdb91b2d989f1208edd44202aa2d2d87
jianchengwang <jiancheng_wang@yahoo.com>
2019-04-23 20:39:05 +0800

    add netty file code

---
+++ lib/netty/src/main/java/cn/jianchengwang/todo/lib/netty/example/file/FileServerHandler.java
@@ -0,0 +1,52 @@
+package cn.jianchengwang.todo.lib.netty.example.file;
+
+import io.netty.channel.*;
+import io.netty.handler.ssl.SslHandler;
+import io.netty.handler.stream.ChunkedFile;
+
+import java.io.RandomAccessFile;
+
+public class FileServerHandler extends SimpleChannelInboundHandler<String> {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush("HELLO: Type the path of the file to retrieve.\n");
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+
+        RandomAccessFile raf = null;
+        long length = -1;
+        try {
+            raf = new RandomAccessFile(msg, "r");
+            length = raf.length();
+        } catch (Exception e) {
+            ctx.writeAndFlush("ERR: " + e.getClass().getSimpleName() + ": " + e.getMessage() + '\n');
+        } finally {
+            if(length < 0 && raf != null) {
+                raf.close();
+            }
+        }
+
+        ctx.write("OK: " + raf.length() + "\n");
+        if(ctx.pipeline().get(SslHandler.class) == null) {
+            // SSL not enabled - can use zero-copy file transfer.
+            ctx.write(new DefaultFileRegion(raf.getChannel(), 0, length));
+        } else {
+            // SSL enabled - can't use zero-copy file transfer
+            ctx.write(new ChunkedFile(raf));
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+
+        if (ctx.channel().isActive()) {
+            ctx.writeAndFlush("ERR: " +
+                    cause.getClass().getSimpleName() + ": " +
+                    cause.getMessage() + '\n').addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+}
