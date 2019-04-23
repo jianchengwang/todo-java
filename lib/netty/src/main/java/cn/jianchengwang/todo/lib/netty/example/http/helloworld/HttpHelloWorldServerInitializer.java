4abef3c861677003e00a1e0729926190b3918872
jianchengwang <jiancheng_wang@yahoo.com>
2019-04-24 00:14:08 +0800

    add netty http(cors,file,helloworld) code

---
+++ lib/netty/src/main/java/cn/jianchengwang/todo/lib/netty/example/http/helloworld/HttpHelloWorldServerInitializer.java
@@ -0,0 +1,30 @@
+package cn.jianchengwang.todo.lib.netty.example.http.helloworld;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.HttpServerExpectContinueHandler;
+import io.netty.handler.ssl.SslContext;
+
+public class HttpHelloWorldServerInitializer extends ChannelInitializer {
+
+    private final SslContext sslCtx;
+
+    public HttpHelloWorldServerInitializer(SslContext sslCtx) {
+        this.sslCtx = sslCtx;
+    }
+
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+
+        ChannelPipeline p = ch.pipeline();
+        if(sslCtx != null) {
+            p.addLast(sslCtx.newHandler(ch.alloc()));
+        }
+        p.addLast(new HttpServerCodec());
+        p.addLast(new HttpServerExpectContinueHandler());
+        p.addLast(new HttpHelloWorldServerHandler());
+
+    }
+}
