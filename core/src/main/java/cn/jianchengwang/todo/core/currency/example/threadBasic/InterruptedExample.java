bd2f6ee9c90b23477c7a31bac270a7fefb710831
王建城 <jiancheng_wang@yahoo.com>
2019-06-20 18:25:50 +0800

    add thread basic

---
+++ core/src/main/java/cn/jianchengwang/todo/core/currency/example/threadBasic/InterruptedExample.java
@@ -0,0 +1,38 @@
+package cn.jianchengwang.todo.core.currency.example.threadBasic;
+
+public class InterruptedExample {
+
+    public static void main(String[] args) {
+
+        final Thread sleepThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                super.run();
+            }
+        };
+
+        Thread busyThread = new Thread() {
+            @Override
+            public void run() {
+                while (true);
+            }
+        };
+
+        sleepThread.start();
+        busyThread.start();
+
+        sleepThread.interrupt();
+        busyThread.interrupt();
+
+        while (sleepThread.isInterrupted());
+
+        System.out.println("sleepThread isInterrupted: " + sleepThread.isInterrupted());
+        System.out.println("busyThread isInterrupted: " + busyThread.isInterrupted());
+    }
+
+}
