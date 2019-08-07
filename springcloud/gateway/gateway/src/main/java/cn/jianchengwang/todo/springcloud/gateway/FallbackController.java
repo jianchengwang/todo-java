7a34c985377e0ee8a89a0a2c3e19625675a446e6
jiancheng_wang <jiancheng_wang@yahoo.com>
2019-08-07 17:02:21 +0800

    add gateway

---
+++ springcloud/gateway/gateway/src/main/java/cn/jianchengwang/todo/springcloud/gateway/FallbackController.java
@@ -0,0 +1,17 @@
+package cn.jianchengwang.todo.springcloud.gateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: yibo
+ **/
+@RestController
+public class FallbackController {
+
+    @GetMapping("/fallback")
+    public String fallback() {
+        return "Hello World!\nfrom gateway";
+    }
+
+}
---
+++ springcloud/gateway/producer/src/main/java/cn/jianchengwang/todo/springcloud/gateway/ProducerApplication.java
@@ -0,0 +1,16 @@
+package cn.jianchengwang.todo.springcloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yibo
+ */
+@SpringBootApplication
+public class ProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+
+}
