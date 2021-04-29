ca402b5f7adede15fd3968e443d89c5e420db386
jianchengwang <jiancheng_wang@yahoo.com>
2021-04-30 01:20:54 +0800

    add bigdata flink-scala-demo

---
+++ bigdata/flink-scala-demo/src/test/scala/todo/java/bigdata/AppTest.scala
@@ -0,0 +1,17 @@
+package todo.java.bigdata
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
